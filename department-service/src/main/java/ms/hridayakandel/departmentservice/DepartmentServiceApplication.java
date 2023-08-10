fbb621886e226144bdc7c0829d03f36d0595ca1f
hridayakandel <hridayakandel@gmail.com>
2023-08-10 00:19:54 -0500

    📚 Add API Documentation with SpringDoc OpenAPI in Department Service
    This commit implements comprehensive API documentation using SpringDoc OpenAPI in the Department service.
    
    🔗 Tasks Completed:
    - Added SpringDoc OpenAPI dependency
    - Thoroughly tested APIs using the Swagger URL
    - Defined clear general API information
    - Enhanced Swagger API documentation with Annotations for REST APIs
    - Improved Swagger API documentation with Annotations for Models
    
    These changes enhance the documentation quality, making it easier for developers to understand the APIs and models.
    
    #documentation #API #SpringDocOpenAPI

--- department-service/src/main/java/ms/hridayakandel/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/ms/hridayakandel/departmentservice/DepartmentServiceApplication.java
@@ -1,14 +1,38 @@
 package ms.hridayakandel.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs Documentation",
+				description = "Department service REST APIs Documentation",
+				version = "v1.0",
+				contact=@Contact(
+						name = "Hridaya",
+						email = "hridayakandel@gmail.com"
+				),
+				license = @License(
+						name="Apache 2.0",
+						url= "https://www.hridayakandel.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-service docs",
+				url = "https://www.hridayakandel.com"
+		)
 
+)
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class DepartmentServiceApplication {
 	@Bean
 	public ModelMapper modelMapper(){
--- department-service/src/main/java/ms/hridayakandel/departmentservice/controller/departmentController.java
+++ department-service/src/main/java/ms/hridayakandel/departmentservice/controller/departmentController.java
@@ -1,5 +1,8 @@
 package ms.hridayakandel.departmentservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import ms.hridayakandel.departmentservice.dto.DepartmentDto;
 import ms.hridayakandel.departmentservice.service.DepartmentService;
@@ -7,6 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name ="Department Service - DepartmentController",
+        description = "Department Controller Exposes REST APIs for department-services"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/departments")
@@ -14,6 +21,14 @@ public class departmentController {
     private DepartmentService departmentService;
 
     //saving Department REST Api
+    @Operation(
+            summary = "Save department REST API",
+            description = "Save Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<?> saveDepartment(@RequestBody DepartmentDto departmentDto){
         DepartmentDto saveDepartmentDto = departmentService.saveDepartment(departmentDto);
@@ -22,6 +37,14 @@ public class departmentController {
 
     }
     //get Department by code REST Api
+    @Operation(
+            summary = "Get department REST API",
+            description = "Get Department REST API is used to get  a department object from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status 200 SUCCESS"
+    )
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartmentByDepatmentCode(@PathVariable("department-code")  String departmentCode){
         DepartmentDto departmentDto = departmentService.getDepartmentByDepartmentCode(departmentCode);
